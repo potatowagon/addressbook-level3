06e420f5e7d4c9846348a5cd20892f0653dcb59f
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-09-21 15:13:57 +0800

    Add EditCommand, fixed StorageStub

---
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.commands;
+
+public class EditCommand extends Command {
+
+	@Override
+	public CommandResult execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
