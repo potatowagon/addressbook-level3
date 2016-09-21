91688ec3c872936a4ca8ab1f64176c402f0de0b0
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-09-21 15:03:16 +0800

    Create StorageStub

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,78 @@
+package seedu.addressbook.storage;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Paths;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.Storage.StorageOperationException;
+import seedu.addressbook.storage.jaxb.AdaptedAddressBook;
+
+public class StorageStub extends Storage {
+	private final JAXBContext jaxbContext;
+
+
+	@Override
+	public void save(AddressBook addressBook) throws StorageOperationException {
+
+		try {
+			path = Paths.get(DEFAULT_STORAGE_FILEPATH);
+			final Writer fileWriter = new BufferedWriter(new FileWriter(path.toFile()));
+
+		} catch (IOException ioe) {
+			throw new StorageOperationException("Error writing to file: " + path + " error: " + ioe.getMessage());
+
+		}
+	}
+
+	@Override
+	public AddressBook load() throws StorageOperationException {
+        try (final Reader fileReader =
+                     new BufferedReader(new FileReader(path.toFile()))) {
+
+            final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            final AdaptedAddressBook loaded = (AdaptedAddressBook) unmarshaller.unmarshal(fileReader);
+            // manual check for missing elements
+            if (loaded.isAnyRequiredFieldMissing()) {
+                throw new StorageOperationException("File data missing some elements");
+            }
+            return loaded.toModelType();
+
+        /* Note: Here, we are using an exception to create the file if it is missing. However, we should minimize
+         * using exceptions to facilitate normal paths of execution. If we consider the missing file as a 'normal'
+         * situation (i.e. not truly exceptional) we should not use an exception to handle it.
+         */
+
+        // create empty file if not found
+        } catch (FileNotFoundException fnfe) {
+            final AddressBook empty = new AddressBook();
+            save(empty);
+            return empty;
+
+        // other errors
+        } catch (IOException ioe) {
+            throw new StorageOperationException("Error writing to file: " + path);
+        } catch (JAXBException jaxbe) {
+            throw new StorageOperationException("Error parsing file data format");
+        } catch (IllegalValueException ive) {
+            throw new StorageOperationException("File contains illegal data values; data type constraints not met");
+        }
+    }
+	
+	@Override
+	public String getPath() {
+		return DEFAULT_STORAGE_FILEPATH;
+	}
+
+}
