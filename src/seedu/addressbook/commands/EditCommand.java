8f7dd856f6cc79a09cb0898df5e17153d2fa46ff
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-09-21 16:03:26 +0800

    EditCommand

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -1,6 +1,14 @@
 package seedu.addressbook.commands;
 
 public class EditCommand extends Command {
+	 public static final String COMMAND_WORD = "edit";
+
+	    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+	            + "Edits the person identified by the index number used in the last person listing.\n\t"
+	            + "Parameters: INDEX\n\t prefix of field To Edit with updated details\n\t"
+	            + "Example: " + COMMAND_WORD + " 1" + " p/81234567";
+
+	    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Edited Person: %1$s";
 
 	@Override
 	public CommandResult execute() {
